017f97c61d3d184b9d0efeed626392970e5e5205
Leo Basov <basov.leo@gmail.com>
2021-01-05 16:43:39 +0100

    Fixed file name.

--- test/unit_tests/mesh/mesh_algorithms.h
+++ test/unit_tests/mesh/mesh_algorithms.h
@@ -8,7 +8,7 @@
 namespace hamt {
 
 TEST(mesh_algorithms, MSH2ToMesh2DRegular) {
-    const std::string file_name("../../../hamt/test/unit_tests/mesh/test_data/block_3.msh");
+    const std::string file_name("./test/unit_tests/mesh/test_data/block_3.msh");
     const gmsh::MSH2 msh2_mesh = gmsh::ReadMSH2(file_name);
     const Mesh2DRegular mesh = mesh_algorithms::MSH2ToMesh2DRegular(msh2_mesh);
 
