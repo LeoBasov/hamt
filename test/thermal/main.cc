28e063292027fee67b0ece8158a7936f34ef3d5e
Leo Basov <basov.leo@gmail.com>
2021-01-07 13:03:30 +0100

    Added thermal test case.

--- test/thermal/main.cc
+++ test/thermal/main.cc
@@ -3,21 +3,36 @@
 #include "../../src/io/gmsh/gmsh.h"
 #include "../../src/io/vtk/unstructured_grid.h"
 #include "../../src/mesh/mesh_algorithms.h"
+#include "../../src/solver/heat_equation.h"
 
 using namespace hamt;
+using namespace Eigen;
 
 int main(int, char **) {
-    const std::string file_name("../../../hamt/test/test_data/block.msh"), file_name_out("./test.vtu");
+    const std::string file_name("../../../hamt/test/test_data/block_fine.msh"), file_name_out("./test.vtu");
     const gmsh::MSH2 msh2_mesh = gmsh::ReadMSH2(file_name);
-    const Mesh2DRegular mesh = mesh_algorithms::MSH2ToMesh2DRegular(msh2_mesh);
-    std::pair<std::string, std::vector<double>> poinst_data{"test_data", std::vector<double>(mesh.nodes_.size(), 10)};
-    std::pair<std::string, std::vector<double>> poinst_data1{"test_1", std::vector<double>(mesh.nodes_.size())};
+    Mesh2DRegular mesh = mesh_algorithms::MSH2ToMesh2DRegular(msh2_mesh);
+    std::pair<MatrixXd, VectorXd> mat_b;
+    std::pair<std::string, std::vector<double>> poinst_data{"temeprature", std::vector<double>(mesh.nodes_.size())};
 
-    for (uint i = 0; i < poinst_data1.second.size(); i++) {
-        poinst_data1.second.at(i) = i;
+    // sides
+    mesh.SetBoundaryType("rs", Mesh2DRegular::DIRICHLET);
+    mesh.SetBoundaryValue("rs", 400);
+
+    mesh.SetBoundaryType("rb", Mesh2DRegular::DIRICHLET);
+    mesh.SetBoundaryValue("rb", 200);
+
+    // surface
+    // mesh.SetSurfaceThermalConductivity("surf", 1);
+
+    mat_b = heat_equation::homogeneous::ConvertMesh2dRegularCartesian(mesh);
+    VectorXd x = mat_b.first.colPivHouseholderQr().solve(mat_b.second);
+
+    for (uint i = 0; i < mesh.nodes_.size(); i++) {
+        poinst_data.second.at(i) = x(i);
     }
 
-    // vtk::unstructured_grid::WriteMesh2DRegular(mesh, file_name_out, {poinst_data, poinst_data1});
+    vtk::unstructured_grid::WriteMesh2DRegular(mesh, file_name_out, {poinst_data});
 
     std::cout << "DONE" << std::endl;
 
