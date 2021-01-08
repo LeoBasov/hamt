227a61231c8bd06eef9c160ca5110a392a08ea2a
Leo Basov <basov.leo@gmail.com>
2021-01-08 13:29:28 +0100

    Added test data.

--- test/unit_tests/main.cc
+++ test/unit_tests/main.cc
@@ -3,6 +3,7 @@
 #include "io/gmsh/gmsh.h"
 #include "mesh/mesh2d_regular.h"
 #include "mesh/mesh_algorithms.h"
+#include "solver/heat_equation/homogeneous.h"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
---
+++ test/unit_tests/solver/heat_equation/homogeneous.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include "../../../../src/solver/heat_equation/homogeneous.h"
+
+namespace hamt {
+
+TEST(heat_equation_homogeneous, ConvertButtomLeft) {}
+
+}  // namespace hamt
