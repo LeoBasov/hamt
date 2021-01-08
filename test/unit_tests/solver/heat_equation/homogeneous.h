4e4c5132e794741a1fd361a2b3336cda5439d07f
Leo Basov <basov.leo@gmail.com>
2021-01-08 13:44:06 +0100

    Extended unit test.

--- test/unit_tests/solver/heat_equation/homogeneous.h
+++ test/unit_tests/solver/heat_equation/homogeneous.h
@@ -7,12 +7,22 @@
 #include "../../../../src/solver/heat_equation/homogeneous.h"
 
 namespace hamt {
+using namespace Eigen;
 
-TEST(heat_equation_homogeneous, ConvertButtomLeft) {
+std::pair<MatrixXd, VectorXd> GetMeshPair() {
     const std::string file_name("../../../hamt/test/test_data/block_4_segments.msh");
     // const std::string file_name("./test/test_data/block_4_segments.msh");
     const gmsh::MSH2 msh2_mesh = gmsh::ReadMSH2(file_name);
     const Mesh2DRegular mesh = mesh_algorithms::MSH2ToMesh2DRegular(msh2_mesh);
+
+    std::pair<MatrixXd, VectorXd> mat_b;
+
+    mat_b.first = MatrixXd::Zero(mesh.nodes_.size(), mesh.nodes_.size());
+    mat_b.second = VectorXd::Zero(mesh.nodes_.size());
+
+    return mat_b;
 }
+
+TEST(heat_equation_homogeneous, ConvertButtomLeft) { std::pair<MatrixXd, VectorXd> mat_b(GetMeshPair()); }
 
 }  // namespace hamt
