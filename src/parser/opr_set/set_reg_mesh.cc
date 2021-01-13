b8e48951e9a651d11f112c8b44ee8fc44997696a
Leo Basov <basov.leo@gmail.com>
2021-01-13 08:29:22 +0100

    Implemented set value for boundary.

--- src/parser/opr_set/set_reg_mesh.cc
+++ src/parser/opr_set/set_reg_mesh.cc
@@ -28,8 +28,17 @@ void SetRegularMesh::Execute(const std::vector<std::string>& argv, const bool&) 
             throw CommandError("unknown boundary [" + argv.at(2) + "]", __PRETTY_FUNCTION__);
         }
     } else if (argv.at(1) == "bound_value") {
-        throw IncompleteCodeError(__PRETTY_FUNCTION__);
-    } else if (argv.at(1) == "bound_value") {
+        auto iter(hamt_->data_->mesh2d_regular_.boundary_names_.find(argv.at(2)));
+
+        if (iter != hamt_->data_->mesh2d_regular_.boundary_names_.end()) {
+            hamt_->data_->mesh2d_regular_.boundaries_.at(iter->second).value = std::stod(argv.at(3));
+
+            std::cout << "SET BOUNDARY: " << argv.at(2)
+                      << " TO VALUE: " << hamt_->data_->mesh2d_regular_.boundaries_.at(iter->second).value << std::endl;
+        } else {
+            throw CommandError("unknown boundary [" + argv.at(2) + "]", __PRETTY_FUNCTION__);
+        }
+    } else if (argv.at(1) == "surf_therm_conduct") {
         throw IncompleteCodeError(__PRETTY_FUNCTION__);
     } else {
         throw CommandError("unknown command 'set reg_mesh' [" + argv.at(1) + "]", __PRETTY_FUNCTION__);
