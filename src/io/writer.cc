bb75d4b59a1af4feace6a18e030cbc6be190687b
Leo Basov <basov.leo@gmail.com>
2021-01-13 09:41:12 +0100

    Refactored writer.

--- src/io/writer.cc
+++ src/io/writer.cc
@@ -2,48 +2,60 @@
 
 namespace hamt {
 
-Writer::Writer() {}
+Writer::Writer() {
+    std::map<FileFormat, Config> file_configs;
+
+    // extend here for more file formats
+    file_configs[VTK] = Config();
+
+    // extend here for more meshes
+    configs_[REGULAR] = file_configs;
+}
 
 void Writer::SetData(const std::shared_ptr<Data>& data) { data_ = data; }
 
-void Writer::Initialize(const std::vector<Config>& configs) { configs_ = configs; }
+void Writer::Initialize(const MeshType mesh_type, const FileFormat file_format, const Config& config) {
+    configs_.at(mesh_type).at(file_format) = config;
+}
 
 void Writer::Write(const uint& iter) {
-    for (const Config& config : configs_) {
-        if (!(iter % config.frequency)) {
-            switch (config.mesh_type) {
-                case REGULAR: {
-                    WriteRegularMesh(config);
-                    break;
-                }
-                default: {
-                    throw Exception("Undefined mesh type [" + std::to_string(config.mesh_type) + "].",
-                                    __PRETTY_FUNCTION__);
-                }
+    for (const auto& config_bins : configs_) {
+        switch (config_bins.first) {
+            case REGULAR: {
+                WriteRegularMesh(config_bins.second, iter);
+                break;
+            }
+            default: {
+                throw Exception("Undefined mesh type [" + std::to_string(config_bins.first) + "].",
+                                __PRETTY_FUNCTION__);
             }
         }
     }
 }
 
-void Writer::WriteRegularMesh(const Config& config) {
-    switch (config.file_format) {
-        case VTK: {
-            if (data_->results_.size()) {
-                std::pair<std::string, std::vector<double>> vec{"temperature",
-                                                                std::vector<double>(data_->results_.size())};
+void Writer::WriteRegularMesh(const std::map<FileFormat, Config>& configs, const uint& iter) {
+    for (const auto& bin : configs) {
+        if (!(bin.second.frequency % iter)) {
+            switch (bin.first) {
+                case VTK: {
+                    if (data_->results_.size()) {
+                        std::pair<std::string, std::vector<double>> vec{"temperature",
+                                                                        std::vector<double>(data_->results_.size())};
 
-                for (uint i = 0; i < vec.second.size(); i++) {
-                    vec.second.at(i) = data_->results_(i);
+                        for (uint i = 0; i < vec.second.size(); i++) {
+                            vec.second.at(i) = data_->results_(i);
+                        }
+
+                        vtk::unstructured_grid::WriteMesh2DRegular(data_->mesh2d_regular_, bin.second.file_name, {vec});
+                    } else {
+                        vtk::unstructured_grid::WriteMesh2DRegular(data_->mesh2d_regular_, bin.second.file_name, {});
+                    }
+                    break;
                 }
-
-                vtk::unstructured_grid::WriteMesh2DRegular(data_->mesh2d_regular_, config.file_name, {vec});
-            } else {
-                vtk::unstructured_grid::WriteMesh2DRegular(data_->mesh2d_regular_, config.file_name, {});
+                default: {
+                    throw Exception("Undefined file format [" + std::to_string(bin.first) + "].", __PRETTY_FUNCTION__);
+                }
             }
-            break;
-        }
-        default: {
-            throw Exception("Undefined file format [" + std::to_string(config.mesh_type) + "].", __PRETTY_FUNCTION__);
         }
     }
 }
--- src/io/writer.h
+++ src/io/writer.h
@@ -18,19 +18,20 @@ class Writer {
         FileFormat file_format = VTK;
         uint frequency = 1;
         std::string file_name;
+        bool activated = false;
     };
 
     Writer();
     ~Writer() = default;
 
     void SetData(const std::shared_ptr<Data>& data);
-    void Initialize(const std::vector<Config>& configs);
+    void Initialize(const MeshType mesh_type, const FileFormat file_format, const Config& config);
     void Write(const uint& iter);
 
    private:
     std::shared_ptr<Data> data_;
-    std::vector<Config> configs_;
+    std::map<MeshType, std::map<FileFormat, Config>> configs_;
 
-    void WriteRegularMesh(const Config& config);
+    void WriteRegularMesh(const std::map<FileFormat, Config>& configs, const uint& iter);
 };
 }  // namespace hamt
