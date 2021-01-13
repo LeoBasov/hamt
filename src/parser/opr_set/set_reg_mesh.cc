4df3501345b602724ee555209edfc9ba4f47c758
Leo Basov <basov.leo@gmail.com>
2021-01-13 08:33:17 +0100

    Added function to set boundary property.

--- src/parser/opr_set/set_reg_mesh.cc
+++ src/parser/opr_set/set_reg_mesh.cc
@@ -39,7 +39,17 @@ void SetRegularMesh::Execute(const std::vector<std::string>& argv, const bool&) 
             throw CommandError("unknown boundary [" + argv.at(2) + "]", __PRETTY_FUNCTION__);
         }
     } else if (argv.at(1) == "surf_therm_conduct") {
-        throw IncompleteCodeError(__PRETTY_FUNCTION__);
+        auto iter(hamt_->data_->mesh2d_regular_.surface_names_.find(argv.at(2)));
+
+        if (iter != hamt_->data_->mesh2d_regular_.surface_names_.end()) {
+            hamt_->data_->mesh2d_regular_.surfaces_.at(iter->second).thermal_conductivity = std::stod(argv.at(3));
+
+            std::cout << "SET SURFACE: " << argv.at(2)
+                      << " TO VALUE: " << hamt_->data_->mesh2d_regular_.surfaces_.at(iter->second).thermal_conductivity
+                      << std::endl;
+        } else {
+            throw CommandError("unknown surface [" + argv.at(2) + "]", __PRETTY_FUNCTION__);
+        }
     } else {
         throw CommandError("unknown command 'set reg_mesh' [" + argv.at(1) + "]", __PRETTY_FUNCTION__);
     }
