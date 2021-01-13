15815c27e8e5a6f0a1946d5fe5704188f6b15509
Leo Basov <basov.leo@gmail.com>
2021-01-13 09:04:59 +0100

    Implemented base writer.

---
+++ src/io/writer.cc
@@ -0,0 +1,39 @@
+#include "writer.h"
+
+namespace hamt {
+
+Writer::Writer() {}
+
+void Writer::SetData(const std::shared_ptr<Data>& data) { data_ = data; }
+
+void Writer::Initialize(const std::vector<Config>& configs) { configs_ = configs; }
+
+void Writer::Write(const uint& iter) {
+    for (const Config& config : configs_) {
+        if (!(iter % config.frequency)) {
+            switch (config.mesh_type) {
+                case REGULAR: {
+                    WriteRegularMesh(config);
+                    break;
+                }
+                default: {
+                    throw Exception("Undefined mesh type [" + std::to_string(config.mesh_type) + "].",
+                                    __PRETTY_FUNCTION__);
+                }
+            }
+        }
+    }
+}
+
+void Writer::WriteRegularMesh(const Config& config) {
+    switch (config.file_format) {
+        case VTK: {
+            break;
+        }
+        default: {
+            throw Exception("Undefined file format [" + std::to_string(config.mesh_type) + "].", __PRETTY_FUNCTION__);
+        }
+    }
+}
+
+}  // namespace hamt
---
+++ src/io/writer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+
+#include "../hamt/data.h"
+
+namespace hamt {
+using namespace Eigen;
+class Writer {
+   public:
+    enum MeshType { REGULAR };
+    enum FileFormat { VTK };
+
+    struct Config {
+        MeshType mesh_type = REGULAR;
+        FileFormat file_format = VTK;
+        uint frequency = 1;
+        std::string file_name;
+    };
+
+    Writer();
+    ~Writer() = default;
+
+    void SetData(const std::shared_ptr<Data>& data);
+    void Initialize(const std::vector<Config>& configs);
+    void Write(const uint& iter);
+
+   private:
+    std::shared_ptr<Data> data_;
+    std::vector<Config> configs_;
+
+    void WriteRegularMesh(const Config& config);
+};
+}  // namespace hamt
