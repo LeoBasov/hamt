caee754382a63e962e60e33e6c872af062854816
Leo Basov <basov.leo@gmail.com>
2021-04-01 11:52:27 +0200

    Extended pybind11 main.

--- pybind11/main.cc
+++ pybind11/main.cc
@@ -8,7 +8,31 @@ PYBIND11_MODULE(pyhamt, m) {
     py::class_<hamt::HAMT>(m, "HAMT")
         .def(py::init<>())
         .def_readwrite("reader", &hamt::HAMT::reader_)
-        .def_readwrite("writer", &hamt::HAMT::writer_);
+        .def_readwrite("writer", &hamt::HAMT::writer_)
+        .def_readwrite("solver", &hamt::HAMT::solver_);
+
+    py::class_<hamt::Solver> solver(m, "Solver");
+    solver.def(py::init<>())
+        .def("execute", &hamt::Solver::Execute)
+        .def_property("config", &hamt::Solver::GetConfig, &hamt::Solver::SetConfig);
+
+    py::enum_<hamt::Solver::SolverType>(solver, "SolverType")
+        .value("HOMOGENEOUS", hamt::Solver::HOMOGENEOUS)
+        .export_values();
+
+    py::enum_<hamt::Solver::CoordinatesType>(solver, "CoordinatesType")
+        .value("CARTESIAN", hamt::Solver::CARTESIAN)
+        .value("CYLINDER", hamt::Solver::CYLINDER)
+        .export_values();
+
+    py::enum_<hamt::Solver::MeshType>(solver, "MeshType").value("REGULAR", hamt::Solver::REGULAR).export_values();
+
+    py::class_<hamt::Solver::Config>(m, "SolverConfig")
+        .def(py::init<>())
+        .def_readwrite("solver_type", &hamt::Solver::Config::solver_type)
+        .def_readwrite("mesh_type", &hamt::Solver::Config::mesh_type)
+        .def_readwrite("coord_type", &hamt::Solver::Config::coord_type)
+        .def_readwrite("activate", &hamt::Solver::Config::activate);
 
     py::class_<hamt::Reader>(m, "Reader").def(py::init<>()).def("read_reg_mesh", &hamt::Reader::ReadRegularMesh);
 
@@ -17,7 +41,7 @@ PYBIND11_MODULE(pyhamt, m) {
         .def("write", &hamt::Writer::Write)
         .def_property("config", &hamt::Writer::GetConfig, &hamt::Writer::SetConfig);
 
-    py::class_<hamt::Writer::Config>(m, "Config")
+    py::class_<hamt::Writer::Config>(m, "WriterConfig")
         .def(py::init<>())
         .def_readwrite("mesh_type", &hamt::Writer::Config::mesh_type)
         .def_readwrite("file_format", &hamt::Writer::Config::file_format)
