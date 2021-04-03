a1b3c22c1abb6be9950625b1f610570ef7882569
Leo Basov <basov.leo@gmail.com>
2021-04-03 14:44:05 +0200

    Updated setup file.

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ elif [ "$1" == "-pybind11" ]; then
     sudo apt install -y python3
     sudo apt install -y python3-pip
     pip3 install "pybind11[global]"
-    pip3 install setuptools
+    pip3 install importlib
     echo "INSTALLATION COMPLETE"
 else
     echo "INSTALLATION COMPLETE"
