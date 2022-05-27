50f300b4d589abf8abed994271077c939763671e
Leo Basov <you@example.com>
2022-05-27 12:19:33 +0200

    updated setup.py for pyhamt

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name='pyhamt',
-    version='0.0.1',
+    version='1.4.0',
+    author='Leo Basov',
+    licence='GNU General Public License v3.0',
     packages=['pyhamt'],
     package_data={'pyhamt': ['pyhamt*']},
     include_package_data=True,
