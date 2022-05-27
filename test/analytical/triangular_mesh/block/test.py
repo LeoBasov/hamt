6ad800c4dc5ec0c77147693b49045eaada468f74
Leo Basov <you@example.com>
2022-05-27 15:49:50 +0200

    extended block test

--- test/analytical/triangular_mesh/block/test.py
+++ test/analytical/triangular_mesh/block/test.py
@@ -1,4 +1,56 @@
 import pyhamt as ph
 
+def set_up_hamt():
+	hamt = ph.HAMT()
+	writer_config = ph.WriterConfig()
+	solver_config = ph.SolverConfig()
+
+	solver_config.mesh_type = ph.Solver.MeshType.TRIANGULAR
+
+	writer_config.file_name = "block"
+	writer_config.activated = True
+
+	hamt.solver.config = solver_config
+
+	hamt.writer.set_config(ph.Writer.MeshType.TRIANGULAR, ph.Writer.FileFormat.CSV, writer_config)
+
+	hamt.reader.read_triangl_mesh("block.msh")
+
+	return hamt
+
+def test_temp_homogene_vertical():
+	hamt = set_up_hamt()
+
+	# setting boundary types
+	hamt.data.mesh2d_triangular.set_boundary_type("l_buttom_left", ph.Mesh2DTriangular.BoundaryType.DIRICHLET)
+	hamt.data.mesh2d_triangular.set_boundary_type("l_buttom_right", ph.Mesh2DTriangular.BoundaryType.DIRICHLET)
+
+	hamt.data.mesh2d_triangular.set_boundary_type("l_right_buttom", ph.Mesh2DTriangular.BoundaryType.NEUMANN)
+	hamt.data.mesh2d_triangular.set_boundary_type("l_right_top", ph.Mesh2DTriangular.BoundaryType.NEUMANN)
+
+	hamt.data.mesh2d_triangular.set_boundary_type("l_top_right", ph.Mesh2DTriangular.BoundaryType.DIRICHLET)
+	hamt.data.mesh2d_triangular.set_boundary_type("l_top_left", ph.Mesh2DTriangular.BoundaryType.DIRICHLET)
+
+	hamt.data.mesh2d_triangular.set_boundary_type("l_left_top", ph.Mesh2DTriangular.BoundaryType.NEUMANN)
+	hamt.data.mesh2d_triangular.set_boundary_type("l_left_buttom", ph.Mesh2DTriangular.BoundaryType.NEUMANN)
+
+	# setting boundary values
+	hamt.data.mesh2d_triangular.set_boundary_value("l_buttom_left", 100.0)
+	hamt.data.mesh2d_triangular.set_boundary_value("l_buttom_right", 100.0)
+
+	hamt.data.mesh2d_triangular.set_boundary_value("l_right_buttom", 0.0)
+	hamt.data.mesh2d_triangular.set_boundary_value("l_right_top", 0.0)
+
+	hamt.data.mesh2d_triangular.set_boundary_value("l_top_right", 300.0)
+	hamt.data.mesh2d_triangular.set_boundary_value("l_top_left", 300.0)
+
+	hamt.data.mesh2d_triangular.set_boundary_value("l_left_top", 0.0)
+	hamt.data.mesh2d_triangular.set_boundary_value("l_left_buttom", 0.0)
+
+	hamt.solver.execute()
+	hamt.writer.write()
+
 if __name__ == '__main__':
-    print('test')
+	print('running test: test_temp_homogene_vertical')
+	test_temp_homogene_vertical()
+	print('done')
